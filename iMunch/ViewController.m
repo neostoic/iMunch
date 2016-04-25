b24f975a0adc53cadffa80a15cbc9466f3cfeeb0
ITP <ITP@guest-wireless-upc-1607-10-120-124-028.usc.edu>
2016-04-25 13:09:24 -0700

    Initial Commit

---
+++ iMunch/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iMunch
+//
+//  Created by ITP on 4/25/16.
+//  Copyright © 2016 Ananth Venkateswaran. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ iMunch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iMunch
+//
+//  Created by ITP on 4/25/16.
+//  Copyright © 2016 Ananth Venkateswaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
